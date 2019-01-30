0ec3b48f8acf4b4e3733bfd72989671c07825591
Civoremo <omerovic_n@hotmail.com>
2019-01-30 17:53:20 -0500

    dashboard test done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "react-testing-library";
 import "react-testing-library/cleanup-after-each";
 import "jest-dom/extend-expect";
+// import "jest-dom";
 
 import Display from "./Display";
 
@@ -37,13 +38,37 @@ describe("Display tests", () => {
         });
     });
 
-    describe("which class", () => {
+    describe("which classname is rendered", () => {
         it("renders red-led when closed", () => {
             const { getByText } = render(<Display closed={true} />);
 
-            const closedLabel = getByText("Closed");
+            const closed = getByText(/closed/i);
+            expect(closed).toHaveClass("red-led");
+        });
 
-            expect(closedLabel).toHaveClass(/led red-led/i);
+        it("renders green-led when open", () => {
+            const { getByText } = render(<Display closed={false} />);
+
+            const open = getByText(/open/i);
+            expect(open).toHaveClass("green-led");
+        });
+
+        it("renders red-led when locked", () => {
+            const { getByText } = render(
+                <Display locked={true} closed={true} />
+            );
+
+            const locked = getByText(/locked/i);
+            expect(locked).toHaveClass("red-led");
+        });
+
+        it("renders green-led when lock open", () => {
+            const { getByText } = render(
+                <Display locked={false} closed={true} />
+            );
+
+            const locked = getByText(/lock/i);
+            expect(locked).toHaveClass("green-led");
         });
     });
 });
