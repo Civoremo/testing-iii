135aeae1d0e419192fb622633a9dbc8d66f9c4f6
Civoremo <omerovic_n@hotmail.com>
2019-01-30 18:27:19 -0500

    cleaned up control test file

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -44,47 +44,4 @@ describe("Control Component Tests", () => {
         const closeButton = getByText(/open gate/i);
         expect(closeButton).toBeDisabled();
     });
-
-    // it("renders unlock and open gates", () => {
-    //     const { getByText } = render(<Controls locked={true} closed={true} />);
-
-    //     getByText(/unlock gate/i);
-    //     getByText(/open gate/i);
-    // });
-
-    // it("renders lock and open gates", () => {
-    //     const { getByText } = render(<Controls locked={false} closed={true} />);
-
-    //     getByText(/lock gate/i);
-    //     getByText(/open gate/i);
-    // });
-
-    // it("renders unlock and close gates", () => {
-    //     const { getByText } = render(<Controls locked={true} closed={false} />);
-
-    //     getByText(/unlock gate/i);
-    //     getByText(/close gate/i);
-    // });
-
-    // describe("Control Button events", () => {
-    //     it("renders open gate button", () => {
-    //         const { getByText } = render(<Controls />);
-
-    //         const button = getByText(/close gate/i);
-
-    //         fireEvent.click(button);
-
-    //         getByText(/open gate/i);
-    //     });
-
-    // it("renders unlocked when button clicked", () => {
-    //     const { getByText } = render(<Controls />);
-
-    //     const button = getByText(/close gate/i);
-
-    //     fireEvent.click(button);
-    //     fireEvent.click(button);
-    //     getByText(/close gate/i);
-    // });
-    // });
 });
