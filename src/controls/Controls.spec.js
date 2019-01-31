ef69f32a289274751b878de5047ee2fdb0b1764b
Civoremo <omerovic_n@hotmail.com>
2019-01-30 19:09:47 -0500

    all test done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from "react";
-import { render, fireEvent } from "react-testing-library";
+import { render } from "react-testing-library";
 import "react-testing-library/cleanup-after-each";
 import "jest-dom/extend-expect";
 
